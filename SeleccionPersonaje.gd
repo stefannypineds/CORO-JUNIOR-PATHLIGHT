7f3d78f746869c8c7a4c5bce35d3d9f0c1d02416
emmanuelsantiago0724 <emmanuelsantiago0724@gmail.com>
2026-04-14 15:05:26 -0500

    Ayuda

--- SeleccionPersonaje.gd
+++ SeleccionPersonaje.gd
@@ -1,9 +1,8 @@
 extends Control
 
 @onready var fondo = $TextureRect
-@onready var timer_espera = $Timer # Asegúrate de que el nodo se llame Timer
 
-# Cargamos las imágenes (revisa que las rutas en tu carpeta sean estas)
+# Imágenes de fondo
 var img_normal = preload("res://Imagenes del recorrido.png")
 var img_theo_brilla = preload("res://Imagenes del recorrido (1).png")
 var img_joy_brilla = preload("res://Imagenes del recorrido (2).png")
@@ -19,9 +18,10 @@ func _on_theo_button_mouse_exited():
 	fondo.texture = img_normal
 
 func _on_theo_button_pressed():
+	# 1. Guardamos la elección
 	Global.personaje_seleccionado = "Theo"
-	fondo.texture = img_theo_brilla
-	timer_espera.start() # ESTO activa el conteo de tiempo
+	# 2. Cambiamos de escena inmediatamente
+	get_tree().change_scene_to_file("res://SeleccionNivel.tscn")
 
 # --- FUNCIONES DE JOY ---
 func _on_joy_button_mouse_entered():
@@ -31,12 +31,7 @@ func _on_joy_button_mouse_exited():
 	fondo.texture = img_normal
 
 func _on_joy_button_pressed():
+	# 1. Guardamos la elección
 	Global.personaje_seleccionado = "Joy"
-	fondo.texture = img_joy_brilla
-	timer_espera.start() # ESTO activa el conteo de tiempo
-
-# --- LA PROGRAMACIÓN QUE TE FALTA ---
-func _on_timer_timeout():
-	# Esta es la orden que le dice a Godot qué sigue después
-	# Asegúrate de que tu escena de niveles se llame exactamente así:
+	# 2. Cambiamos de escena inmediatamente
 	get_tree().change_scene_to_file("res://SeleccionNivel.tscn")
---
+++ ayuda.gd
@@ -0,0 +1,35 @@
+extends Control
+
+# Aquí el código ya sabe dónde están tus fotos en el estante
+var fotos = [
+	preload("res://41.png"), # Foto 1 (Página 0)
+	preload("res://42.png"), # Foto 2 (Página 1)
+	preload("res://43.png"), # Foto 3 (Página 2)
+	preload("res://44.png"), # Foto 4 (Página 3)
+	preload("res://45.png")  # Foto 5 (Página 4)
+]
+
+var pagina_actual = 0 # El número de la página que estamos viendo
+
+@onready var nota_visual = $NotaGrande # Tu cuadro de imagen
+
+func _ready():
+	nota_visual.hide() # Que no se vea al empezar
+
+# --- CUANDO TOCAS EL PAPELITO 1 ---
+func _on_button_1_pressed():
+	pagina_actual = 0 # Le decimos: "Es la primera foto"
+	cambiar_foto()
+func _on_flecha_pressed():
+	if pagina_actual < 4: 
+		# Todavía hay notas: pasamos a la siguiente
+		pagina_actual += 1 
+		cambiar_foto()
+	else:
+		# ¡YA NO HAY MÁS NOTAS! (Llegamos a la 45)
+		# En lugar de solo esconderse, nos manda al menú
+		get_tree().change_scene_to_file("res://Menu_de_juego.tscn")
+
+func cambiar_foto():
+	nota_visual.texture = fotos[pagina_actual] # Ponemos la foto que toca
+	nota_visual.show() # Mostramos el cuadro
