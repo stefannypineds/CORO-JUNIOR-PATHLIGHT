a154f83b777cb932234f58a59176e56477a35684
tamycasas10-byte <tamycasas10@gmail.com>
2026-04-15 13:23:18 -0500

    Videos objetos

---
+++ area_3d6.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	
+	# Comprobamos si el jugador hizo clic izquierdo
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		
+		# ¡MAGIA! Esto abre el navegador con el video
+		OS.shell_open("https://youtu.be/L9cNpOnXLbE?si=lcoNuIRPOaWZCDVa")
