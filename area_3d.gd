f1005c64f9a31218e033b19108d0127375ca32e3
tamycasas10-byte <tamycasas10@gmail.com>
2026-04-14 20:36:48 -0500

    Texto pasos

--- area_3d.gd
+++ area_3d.gd
@@ -1,17 +1,17 @@
 extends Area3D
 
-# Esta variable aparecerá en el inspector para que arrastres tu cuadro blanco
+# Esta cajita nos va a pedir el cuadro de texto en el inspector
 @export var cuadro_texto: Control 
 
-func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	# Detectamos el clic izquierdo del mouse
+func _on_input_event(camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	
+	# Comprobamos si el jugador hizo clic izquierdo
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		
+		# Verificamos que el cuadro sí esté conectado en el inspector
 		if cuadro_texto != null:
-			cuadro_texto.show() # Mostramos el cuadro
+			cuadro_texto.show() # Lo hacemos visible
 			
-			# Calculamos la posición 2D encima de la oveja 3D
-			var posicion_pantalla = camera.unproject_position(global_position + Vector3(0, 1.5, 0))
-			
-			# Centramos el cuadro
+			# Calculamos dónde ponerlo (arriba de la oveja y centrado)
+			var posicion_pantalla = (camera as Camera3D).unproject_position(global_position + Vector3(0, 1.5, 0))
 			cuadro_texto.global_position = posicion_pantalla - (cuadro_texto.size / 2.0)
