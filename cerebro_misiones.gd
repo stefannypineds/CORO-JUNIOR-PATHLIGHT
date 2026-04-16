ec0c61ebcdad747f1df764d35c4dadaece1a836c
tamycasas10-byte <tamycasas10@gmail.com>
2026-04-16 15:52:37 -0500

    Nombre usuario

--- cerebro_misiones.gd
+++ cerebro_misiones.gd
@@ -1,11 +1,18 @@
 extends Node
 
+# Esta es la variable nueva para el nombre
+var nombre_jugador : String = ""
+
 # El Cerebro solo conoce a los "Jefes" de cada paquete
-var m1 = MisionBase.new(["Ovejas", "Biblia", "Binoculares", "Pintura2"])
-var m2 = MisionBase.new(["Paso", "Pintura", "Biblia2", "Binoculares2", "Pez"])
-var m3 = MisionBase.new(["Familia orando", "Biblia", "Pintura", "Binoculares"])
-var m4 = MisionBase.new(["Biblia","Binoculares","Pintura","Nube","Moises","David","Jesus"])
-var m5 = MisionBase.new(["Biblia2","Binoculares2","Pintura2","Familia"])
-var m6 = MisionBase.new(["modelo regalo","Rosa","Biblia","Binoculares","Pintura"])
-var m7 = MisionBase.new(["Biblia","Pintura","Canasta peces","Binoculares"])
-var m8 = MisionBase.new(["Biblia2","Pintura2","Binoculares2","Modelo+corazon"])
+var m1 = MisionBase.new(["Ovejas", "Biblia", "Binoculares"])
+var m2 = MisionBase.new(["Paso", "Pintura", "Biblia2"])
+var m3 = MisionBase.new(["Familia orando", "Biblia", "Pintura"])
+var m4 = MisionBase.new(["Biblia", "Binoculares", "Pintura"])
+var m5 = MisionBase.new(["Biblia2", "Binoculares2", "Pintura2"])
+var m6 = MisionBase.new(["modelo regalo", "Rosa", "Biblia"])
+var m7 = MisionBase.new(["Biblia", "Pintura", "Canasta"])
+
+# Aquí abajo debes tener tu función de guardar_partida()
+# No la borres si ya tenías código dentro.
+func guardar_partida():
+	pass
--- registro.gd
+++ registro.gd
@@ -1,22 +1,20 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
-
 
 func _on_button_pressed() -> void:
 	var caja_nombre = $LineEdit
 	
+	# Verificamos que no esté vacío y que el "Cerebro" exista
 	if caja_nombre.text != "":
 		CerebroMisiones.nombre_jugador = caja_nombre.text
 		CerebroMisiones.guardar_partida() 
 		
-		# Teletransporte a tu menú principal (Asegúrate de que el nombre del archivo sea el correcto)
+		# Cambia a la escena del menú (Asegúrate que el nombre sea exacto)
 		get_tree().change_scene_to_file("res://Menu_de_juego.tscn")
+	else:
+		print("Por favor, escribe un nombre")
