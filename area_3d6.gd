fb16db17600c450245488394fcb0b76183519393
user325463 <stephaniepinedaa16@gmail.com>
2026-04-17 16:19:32 -0500

    Mision 4 video

--- area_3d6.gd
+++ area_3d6.gd
@@ -6,6 +6,6 @@ func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3,
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		
 		# ¡MAGIA! Esto abre el navegador con el video
-		OS.shell_open("https://youtu.be/L9cNpOnXLbE?si=lcoNuIRPOaWZCDVa")
+		OS.shell_open("https://youtu.be/TcdTbolUVJE")
 
 	CerebroMisiones.m4.registrar_clic("Binoculares")
