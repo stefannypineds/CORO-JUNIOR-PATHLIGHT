64a090904df8ef8e61c1ae1c097dd2798f6358d9
tamycasas10-byte <tamycasas10@gmail.com>
2026-04-18 13:16:22 -0500

    Insignias

--- texture_rect7.gd
+++ texture_rect7.gd
@@ -3,6 +3,18 @@ extends TextureRect
 func _ready() -> void:
 	# REGLA: Cambia "mX" por la misión de este mapa (m1, m2, m3...)
 	CerebroMisiones.m7.insignia = self
+# Esto es lo nuevo para el nombre y la fecha
+	actualizar_identidad_insignia()
 
+func actualizar_identidad_insignia():
+	# 1. Usamos 'nombre_jugador' que es como aparece en tu CerebroMisiones
+	if CerebroMisiones.nombre_jugador != "":
+		$LabelNombre.text = CerebroMisiones.nombre_jugador
+	else:
+		$LabelNombre.text = "Explorador"
+
+	# 2. Ponemos la fecha del sistema
+	var d = Time.get_date_dict_from_system()
+	$LabelFecha.text = "%02d/%02d/%d" % [d.day, d.month, d.year]
 func _on_button_pressed() -> void:
 	hide() # Replace with function body.
